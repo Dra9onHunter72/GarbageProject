0dcfd7889bd67889ce7810073451e078f0b1724d
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-09-28 12:42:17 -0600

    I added 3 classes

--- src/garbage/controller/GarbageController.java
+++ src/garbage/controller/GarbageController.java
@@ -3,4 +3,9 @@ package garbage.controller;
 public class GarbageController
 {
 
+	public void start()
+	{
+		System.out.println("This is a garbage project");
+		System.out.println("such a waste");
+	}
 }
